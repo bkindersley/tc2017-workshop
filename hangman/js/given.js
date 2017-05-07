03f7c4e18f52d2bcde30b4d8ec5725e1985d805a
bria <bkindersley@gmail.com>
2017-05-06 17:45:40 -0700

    remove extra pages, change win/lose result, neaten, add more comments

--- hangman/js/given.js
+++ hangman/js/given.js
@@ -2,7 +2,7 @@ randomIndex = function(n){
   return Math.floor((Math.random() * n));
 };
 
-updateDisplay = function(){
+updateDisplay = function(curr_word){
   $('#word').text(curr_word.join(" "));
 };
 
@@ -13,9 +13,21 @@ getSubmittedLetter = function(){
 };
 
 endGame = function(state){
-  window.location.replace("./"+state+".html");
+  $('#letter-entry').attr("disabled", "disabled");
+  $('#letter-submit').attr("disabled", "disabled");
+  if (state === "win"){
+    displayMessage("Yay! You won!");
+  }
+  if (state === "lose"){
+    displayMessage("Oh no! You lost!");
+  }
 };
 
 newImage = function(i){
   $('.hangman').attr("src", ".\\img\\hangman"+i+".png");
 };
+
+displayMessage = function(msg){
+  var pgph = $('<p class = "message">' + msg + '</p>');
+  $('#message-container').append(pgph);
+}
--- hangman/js/hangman.js
+++ hangman/js/hangman.js
@@ -6,6 +6,14 @@ var words = [
   ['r','i','v','e','r'],
 ];
 
+//i chose to have 6 steps:
+//0: empty
+//1: head
+//2: body
+//3: arm
+//4: arm
+//5: leg
+//6: leg
 const ALLOWED_TRIES = 6;
 
 //this line is part of the set-up
@@ -14,68 +22,66 @@ jQuery(document).ready(function(){
 
   //randomly choose word from the dictionary
   var word_i = randomIndex(words.length);
-  var word = words[word_i];
+  var word = words[word_i];//the randomly chosen word
 
-  //build underscore string
-  var curr_word = [];
+  //build an array of underscores from chosen word
+  var curr_word = [];//tracks the current state of the guesses
   //go through each letter of word and add an
   //underscore to curr_word
   for (i=0; i < word.length; i++){
     curr_word.push('_');
   }
-  //to track how many letters are guessed
-  var progress = word.length;
 
-  //to track how many guesses are used
-  var curr_try = 0;
+  var numBlanksLeft = word.length;//track how many letters are guessed
 
-  //display
-  updateDisplay();
+  var curr_try = 0;//track how many guesses are used
 
-  //what happens when submit button clicked
+  //display the underscores
+  updateDisplay(curr_word);
+
+  //what happens when the submit button is clicked
   sumbitLetter = function(){
     //read letter from text box
     var letter = getSubmittedLetter();
-    //check valid
 
-    //check guessed
-
-    //check in word
-    //to track if letter is in word
-    var isIn = false;
+    //check if the letter is in word
+    var isInWord = false;//track if letter is in word
     //go through each letter of word
     for (i=0;i<word.length;i++){
-      //compare the letter to letter
+      //compare the sumbitted letter to the current letter
       if (letter == word[i]){
         //update curr_word
         curr_word[i] = letter;
-        //update progress
-        progress--;
-        //set isIn true
-        isIn = true;
+        //update numBlanksLeft
+        numBlanksLeft--;
+        //set isInWord true
+        isInWord = true;
       }
     }
     //guessed right
-    if (isIn){
-      updateDisplay()
+    if (isInWord){
+      //update the displayed word
+      updateDisplay(curr_word);
 
-      //wait a little
-      setTimeout(function() {
-        //all letters guessed
-        if(progress==0){
-          endGame("win");
-        }
-      }, 500);
+      //all letters guessed:
+      if(numBlanksLeft==0){
+        //win!
+        endGame("win");
+      }
+      //otherwise:
     }
     //guessed wrong
     else {
       //update try counter
       curr_try++;
-      //still have some tries left
+      //still have some tries left:
       if(curr_try <= ALLOWED_TRIES){
-          newImage(curr_try);
+        //update image
+        newImage(curr_try);
       }
+      //no tries left:
       else{
+        //lose :(
           endGame("lose");
       }
     }
